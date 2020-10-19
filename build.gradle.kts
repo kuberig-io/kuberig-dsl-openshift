a29a7d8d1b011f729b3e134eecd6265a91b1ada6
Tom Eyckmans <teyckmans@gmail.com>
2020-10-19 21:09:17 +0200

    io.kuberig changes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import com.jfrog.bintray.gradle.BintrayExtension
 import io.kuberig.dsl.vanilla.plugin.KubeRigDslVanillaPluginExtension
 import io.kuberig.dsl.vanilla.plugin.SemVersion
 
@@ -8,9 +7,8 @@ buildscript {
         maven("https://dl.bintray.com/teyckmans/rigeldev-oss-maven")
     }
     dependencies {
-        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
         classpath("io.kuberig:kuberig-dsl-generator-gradle-plugin:${version}")
-        classpath("io.kuberig.dsl.vanilla.plugin:kuberig-dsl-vanilla-plugin:0.1.4")
+        classpath("io.kuberig.dsl.vanilla.plugin:kuberig-dsl-vanilla-plugin:0.2.0")
     }
 }
 
@@ -21,66 +19,4 @@ configure<KubeRigDslVanillaPluginExtension> {
     gitHubRepo = "origin"
     startVersion = SemVersion(3, 9, 0)
     swaggerLocation = "api/swagger-spec/openshift-openapi-spec.json"
-}
-
-subprojects {
-    apply {
-        plugin("com.jfrog.bintray")
-        plugin("maven-publish")
-        plugin("java")
-        plugin("idea")
-    }
-
-    val subProject = this
-
-    subProject.group = "io.kuberig.dsl.openshift"
-    subProject.version = project.version
-
-    repositories {
-        jcenter()
-    }
-
-    dependencies {
-        val testImplementation by configurations
-        val testRuntimeOnly by configurations
-
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.2")
-    }
-
-    val sourcesJar by tasks.registering(Jar::class) {
-        archiveClassifier.set("sources")
-
-        val sourceSets: SourceSetContainer by subProject
-        from(sourceSets["main"].allSource)
-    }
-
-    configure<PublishingExtension> {
-
-        publications {
-            register(subProject.name, MavenPublication::class) {
-                from(components["java"])
-                artifact(sourcesJar.get())
-            }
-        }
-
-    }
-
-    configure<BintrayExtension> {
-        val bintrayApiKey : String by subProject
-        val bintrayUser : String by subProject
-
-        user = bintrayUser
-        key = bintrayApiKey
-        publish = true
-
-        pkg(closureOf<BintrayExtension.PackageConfig>{
-            repo = "rigeldev-oss-maven"
-            name = subProject.name
-            setLicenses("Apache-2.0")
-            vcsUrl = "https://github.com/teyckmans/kuberig-dsl-openshift"
-        })
-
-        setPublications(subProject.name)
-    }
 }
