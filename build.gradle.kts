c8d7b886692b4a0a92d45cb920f5f40c787da425
Tom Eyckmans <teyckmans@gmail.com>
2020-10-27 06:36:25 +0100

    Gradle plugin version management improvement (will allow propagating kuberig-dsl-vanilla-plugin version updates automatically).

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,10 @@
-import io.kuberig.dsl.vanilla.plugin.KubeRigDslVanillaPluginExtension
 import io.kuberig.dsl.vanilla.plugin.SemVersion
 
-buildscript {
-    repositories {
-        jcenter()
-        maven("https://dl.bintray.com/teyckmans/rigeldev-oss-maven")
-    }
-    dependencies {
-        classpath("io.kuberig:kuberig-dsl-generator-gradle-plugin:${version}")
-        classpath("io.kuberig.dsl.vanilla.plugin:kuberig-dsl-vanilla-plugin:0.2.4")
-    }
+plugins {
+    id("io.kuberig.dsl.vanilla.plugin")
 }
 
-apply(plugin = "io.kuberig.dsl.vanilla.plugin")
-
-configure<KubeRigDslVanillaPluginExtension> {
+kubeRigDslVanilla {
     gitHubOwner = "openshift"
     gitHubRepo = "origin"
     startVersion = SemVersion(3, 9, 0)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,28 @@
 rootProject.name = "kuberig-dsl-openshift"
 
+pluginManagement {
+    val version : String by settings
+    val kuberigDslVanillaPluginVersion : String by settings
+
+    plugins {
+        id("io.kuberig.dsl.generator") version(version)
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "io.kuberig.dsl.vanilla.plugin") {
+                useModule("io.kuberig.dsl.vanilla.plugin:kuberig-dsl-vanilla-plugin:${kuberigDslVanillaPluginVersion}")
+            }
+        }
+    }
+
+    repositories {
+        jcenter()
+        maven("https://dl.bintray.com/teyckmans/rigeldev-oss-maven")
+        gradlePluginPortal()
+    }
+}
+
 include("kuberig-dsl-openshift-v3.9.0")
 include("kuberig-dsl-openshift-v3.10.0")
 include("kuberig-dsl-openshift-v3.11.0")
